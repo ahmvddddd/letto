06b1821df9c9d67c287f4a54e7e9cd8a4d37df16
belloahmadmm <belloahmadmm@gmail.com>
2025-10-30 00:32:55 +0000

    colors

--- lib/feature/custom_navbar.dart
+++ lib/feature/custom_navbar.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:iconsax/iconsax.dart';
-
 import '../utils/constants/custom_colors.dart';
+import '../utils/constants/custom_sizes.dart';
 import '../utils/helper/helper_functions.dart';
 
 final navigationProvider = StateProvider<int>((ref) => 0);
@@ -25,27 +25,27 @@ class CustomBottomNavBar extends ConsumerWidget {
           child: ClipRRect(
             borderRadius: BorderRadius.circular(30),
             child: BottomNavigationBar(
-            backgroundColor:CustomColors.darkGrey,
+            backgroundColor:dark ? Colors.black : Colors.white,
               currentIndex: navIndex,
               onTap: (index) => ref.read(navigationProvider.notifier).state = index,
               type: BottomNavigationBarType.fixed,
-              selectedItemColor: dark ? Colors.white : Colors.black,
-              unselectedItemColor: CustomColors.darkerGrey,
+              selectedItemColor: CustomColors.primary,
+              unselectedItemColor: dark ? Colors.white : Colors.black,
               elevation: 0,
               items: [
                 BottomNavigationBarItem(
-                  icon: Icon(Icons.home_outlined, color: dark ? CustomColors.darkGrey : CustomColors.darkerGrey,),
-                  activeIcon: Icon(Icons.home, color: dark ? Colors.white : Colors.black,),
+                  icon: Icon(Iconsax.home, size: Sizes.iconM,),
+                  activeIcon: Icon(Iconsax.home_1, color: CustomColors.primary, size: Sizes.iconM,),
                   label: 'Home',
                 ),
                 BottomNavigationBarItem(
-                  icon: Icon(Icons.favorite_border, color: dark ? CustomColors.darkGrey : CustomColors.darkerGrey,),
-                  activeIcon: Icon(Icons.favorite, color: dark ? Colors.white : Colors.black),
+                  icon: Icon(Icons.favorite_border),
+                  activeIcon: Icon(Icons.favorite, color: CustomColors.primary, size: Sizes.iconM,),
                   label: 'Favorites',
                 ),
                 BottomNavigationBarItem(
-                  icon: Icon(Iconsax.user, color: dark ? CustomColors.darkGrey : CustomColors.darkerGrey,),
-                  activeIcon: Icon(Iconsax.user1, color: dark ? Colors.white : Colors.black),
+                  icon: Icon(Iconsax.user, size: Sizes.iconM,),
+                  activeIcon: Icon(Iconsax.user, color: CustomColors.primary, size: Sizes.iconM,),
                   label: 'Profile',
                 ),
               ],
