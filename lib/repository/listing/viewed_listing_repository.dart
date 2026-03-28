26f2cd1f927ea91477e64797e3611c5378c3a711
ahmvddddd <belloahmadmm@gmail.com>
2026-03-28 23:58:15 +0000

    indo practice

--- lib/repository/listing/viewed_listing_repository.dart
+++ lib/repository/listing/viewed_listing_repository.dart
@@ -20,18 +20,3 @@ class ViewedListingRepository {
   });
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
-          const SizedBox(height: 16)
-        ],
-      ),
-    );
-  }
-}
